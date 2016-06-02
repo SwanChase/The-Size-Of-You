dc8c3938f2102541ed39b6671eff97049e84f655
NLferdiNL <nlferdinl@gmail.com>
2016-06-02 08:59:09 +0200

    playermovement bugfix

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D rigidbody;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+}
