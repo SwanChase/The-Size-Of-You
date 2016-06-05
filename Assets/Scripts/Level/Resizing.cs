78e04b56e00458efaec057c83aafc00433022a4e
Swan Chase <swancd@gmail.com>
2016-06-06 01:18:28 +0200

    resizing is now possible
    still need to give better names to scripts and vars
    olso there is no limit to how much you can grow/shrink

---
+++ Assets/Scripts/Level/Resizing.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resizing : MonoBehaviour
+{
+    //make the wall scream out its being touched and make the player listen using Delegates
+    //and in the player make a function for resizing the player
+    [SerializeField]
+    float resizingValue;
+
+    [SerializeField]
+    bool isGrower = false;
+    [SerializeField]
+    bool isShrinker = false;
+
+    public delegate void Resizer(float sizeValue);
+    public static event Resizer Grower;
+    public static event Resizer Shrinker;
+
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.gameObject.tag == Tags.PLAYER)
+        {
+            if (isGrower)
+            {
+                Grower(resizingValue);
+            }
+
+            if (isShrinker)
+            {
+                Shrinker(resizingValue);
+            }
+        }
+    }
+}
--- Assets/Scripts/Player/Resizing.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Resizing : MonoBehaviour {
-
-    //class for the the recieving of the growWall or ShrinkWall Delegates and Changing them acordingly
-}
