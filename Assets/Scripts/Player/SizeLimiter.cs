ac98fbe8581b8e0150977a1034f3c6d0ccb78c22
Swan Chase <swancd@gmail.com>
2016-06-13 02:52:17 +0200

    added scripts
    -script for limiting the size of an object
    -script for limiting the frequency of resizing

---
+++ Assets/Scripts/Player/SizeLimiter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SizeLimiter : MonoBehaviour
+{
+
+    [SerializeField]
+    float minSize;
+    [SerializeField]
+    float maxSize;
+
+    void Update()
+    {
+        transform.localScale = new Vector3(
+              Mathf.Clamp(transform.localScale.x, minSize, maxSize),
+              Mathf.Clamp(transform.localScale.y, minSize, maxSize),
+              Mathf.Clamp(transform.localScale.z, minSize, maxSize));
+
+    }
+}
