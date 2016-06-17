b07a8b81e6a98c46f0567c2a0f11aa3789d3c9bf
Swan Chase <swancd@gmail.com>
2016-06-17 17:19:53 +0200

    setup for sfx olmost done

---
+++ Assets/Scripts/Player/PlayerSFX.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PlayerSFX : MonoBehaviour
+{
+    /*
+    Dictionary<int, AudioClip> playerSFXDict = new Dictionary<int, AudioClip>();
+
+    void Start()
+    {
+        AddToDict(AudioClip.);
+    }
+
+    private void AddToDict(AudioClip sfx)
+    {
+        if (!playerSFXDict.ContainsValue(sfx))
+        {
+            playerSFXDict.Add(0, sfx);
+            Debug.Log("it diden't contain" + sfx);
+        }
+        else
+        {
+            Debug.Log("Already contains" + sfx);
+        }
+    }
+
+    public void PlayAudio(int clip)
+    {
+        AudioSource audiosource;
+        AudioClip[] geluid;
+        audiosource = Camera.main.transform.Find("EmptyCamera").GetComponent<AudioSource>();
+        //AudioSource audio = GetComponent<AudioSource> ();
+        geluid = new AudioClip[]
+        {
+            (AudioClip)Resources.Load ("geluid/ontplof") as AudioClip,
+            (AudioClip)Resources.Load ("geluid/spacesound") as AudioClip,
+            (AudioClip)Resources.Load ("geluid/snoop") as AudioClip
+        };
+        //audio.PlayOneShot (geluid [clip], 0.7f);
+        audiosource.clip = geluid[clip];
+        audiosource.Play();
+    }
+    */
+}
---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume = 0.5f;
+    [Range(-1.5f, 1.5f)]
+    public float pitch = 1f;
+
+    [Range(0f, 1f)]
+    public float randomVolume = 0.1f;
+    [Range(0.5f, 0.5f)]
+    public float randomPitch = 0.1f;
+
+    private AudioSource source;
+
+    public void SetSource(AudioSource _source)
+    {
+        source = _source;
+        source.clip = clip;
+    }
+
+    public void Play()
+    {
+        source.volume = volume * (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f));
+        source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+        source.Play();
+    }
+
+}
+
+public class SoundController : MonoBehaviour
+{
+
+    public static SoundController instance;
+
+    [SerializeField]
+    Sound[] sounds;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Whoops more then one SoundController in scene, beter find it ;) ");
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
+            _go.transform.SetParent(this.transform);
+            sounds[i].SetSource(_go.AddComponent<AudioSource>());
+        }
+
+        PlaySound("GrowSFX");
+    }
+
+    public void PlaySound(string _name)
+    {
+        for (int i = 0; i < sounds.Length; i++)
+        {
+            if (sounds[i].name == _name)
+            {
+                sounds[i].Play();
+                return;
+            }
+        }
+
+        // when there is no sound with _name
+        Debug.LogWarning("AudioController: Sound not Found in list :( , " + _name);
+    }
+
+}
